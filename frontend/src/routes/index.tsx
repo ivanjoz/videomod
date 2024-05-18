4a1c5378f1d3152c29df3641710485225efb6900
ivanjoz <ivan@un.pe>
2024-05-17 22:54:13 -0500

    first commit

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Title } from "@solidjs/meta";
+import Counter from "~/components/Counter";
+import videojs from "video.js";
+import { createEffect } from "solid-js";
+import "video.js/dist/video-js.min.css"
+
+export default function Home() {
+
+  createEffect(() => {
+    videojs(document.getElementById("player-main") as Element)
+  })
+
+  return (
+    <main>
+      <video class="video-js" controls autoplay preload="auto" id="player-main">
+        <source src="/videos/dune_part_2_2024.webm" type="video/webm"/>
+      </video>
+    </main>
+  );
+}
